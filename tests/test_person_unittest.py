c87e6317f2137284a8f91ad6481b8fbdce5ab8f5
Ijeoma Arisa <ijay.arisa@gmail.com>
2025-11-10 13:58:29 -0800

    Added Relationship Enum to person test

--- tests/test_person_unittest.py
+++ tests/test_person_unittest.py
@@ -1,15 +1,15 @@
 import unittest
 
-from models import Person
+from models import Person, Relationship
 
 class TestPerson(unittest.TestCase):
   def setUp(self):
-    self.p = Person("1", "Alice", "Family")
+    self.p = Person("1", "Alice", Relationship.FAMILY)
     
   def test_initial_state(self):
     self.assertEqual(self.p.get_id(), "1")
     self.assertEqual(self.p.get_name(), "Alice")
-    self.assertEqual(self.p.get_relationship(), "Family")
+    self.assertEqual(self.p.get_relationship(), Relationship.FAMILY)
     self.assertEqual(len(self.p.get_prayer_requests()), 0)
     
   def test_id(self):
@@ -21,8 +21,8 @@ class TestPerson(unittest.TestCase):
     self.assertEqual(self.p.get_name(), "Bob")
     
   def test_relationship(self):
-    self.p.set_relationship("Friends")
-    self.assertEqual(self.p.get_relationship(), "Friends")
+    self.p.set_relationship(Relationship.FRIENDS)
+    self.assertEqual(self.p.get_relationship(), Relationship.FRIENDS)
 
   
 if __name__ == "__main__":
