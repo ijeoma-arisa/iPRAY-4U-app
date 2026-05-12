f22286429b5b4575016de059704c92e5def6de06
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-05-11 20:28:18 -0700

    Completed validate_fields tests for missing or invalid name, relationship, and prayer. Updated structure of return values for error helper functions in TestValidateFields. Added 'allow_null' to 'generate_pesron_json' function to enable null check for existing fields' values.

--- tests/helpers/sample_data.py
+++ tests/helpers/sample_data.py
@@ -1,16 +1,23 @@
-def generate_person_json(name=None, relationship=None, prayer=None, person_json=None):
-      person_json = person_json or {}
-      
-      if name is not None:
-          person_json["name"] = name
-      
-      if relationship is not None:
-          person_json["relationship"] = relationship
-          
-      if prayer is not None:
-          person_json["prayer"] = prayer
-          
-      return person_json
+def generate_person_json(name=None, relationship=None, prayer=None, person_json=None, allow_null=False):
+    person_json = person_json or {}
+    
+    if allow_null:
+        return {
+            "name": name,
+            "relationship": relationship,
+            "prayer": prayer
+        }
+    
+    if name is not None:
+        person_json["name"] = name
+    
+    if relationship is not None:
+        person_json["relationship"] = relationship
+        
+    if prayer is not None:
+        person_json["prayer"] = prayer
+        
+    return person_json
   
 def update_existing_json_fields(updated_fields: dict, json: dict):
     for field, updated_value in updated_fields.items():
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -89,6 +89,13 @@ class TestParseRelationship(unittest.TestCase):
         self.assertEqual(parse_relationship(self.field, "pEoPle i KNOW", self.actual_errors), Relationship.KNOWN)
         
         self.assertEqual(len(self.actual_errors), 0)
+        
+    def test_valid_relationship_whitespace(self):
+        self.assertEqual(parse_relationship(self.field, "Friends   ", self.actual_errors), Relationship.FRIENDS)
+        self.assertEqual(parse_relationship(self.field, "\tFamily", self.actual_errors), Relationship.FAMILY)
+        self.assertEqual(parse_relationship(self.field, "People I Know\n\n", self.actual_errors), Relationship.KNOWN)
+        
+        self.assertEqual(len(self.actual_errors), 0)
 
     def test_invalid_integer_input(self):        
         self.assertIsNone(parse_relationship(self.field, 123, self.actual_errors))
@@ -102,16 +109,12 @@ class TestParseRelationship(unittest.TestCase):
         self.assertIsNone(parse_relationship(self.field, ["friends"], self.actual_errors))
         self.assertListEqual(self.actual_errors, self.expected_errors_string)
     
-    def test_invalid_relationship_input_1(self):      
+    def test_invalid_relationship_input_different(self):      
         self.assertIsNone(parse_relationship(self.field, "Customer", self.actual_errors))
         self.assertListEqual(self.actual_errors, self.expected_errors_relationship)
         
-    def test_invalid_relationship_input_2(self):      
+    def test_invalid_relationship_input_similar(self):      
         self.assertIsNone(parse_relationship(self.field, "Friend", self.actual_errors),)
-        self.assertListEqual(self.actual_errors, self.expected_errors_relationship)
-
-    def test_invalid_relationship_input_3(self):      
-        self.assertIsNone(parse_relationship(self.field, "Fam", self.actual_errors))
         self.assertListEqual(self.actual_errors, self.expected_errors_relationship)
     
     def test_invalid_whitespace_input(self):      
@@ -152,7 +155,6 @@ class TestValidateFields(unittest.TestCase):
         valid_relationships = [r.value for r in Relationship]
         return [f"'{f}' must be one of {valid_relationships}" for f in fields]
     
-
     def test_all_fields_valid(self):
         """Test that all fields are parsed and no errors are listed."""
         
@@ -172,20 +174,22 @@ class TestValidateFields(unittest.TestCase):
         missing_name_json = generate_person_json(name=None, relationship="Family", prayer="Strength")
         missing_relationship_json =  generate_person_json(name="Bob", relationship=None, prayer="Strength")
         missing_prayer_json =  generate_person_json(name="Bob", relationship="Family", prayer=None)
-        missing_all_json = generate_person_json(name=None, relationship=None, prayer=None)
+        missing_all_fields_empty_json = generate_person_json(name=None, relationship=None, prayer=None)
+        missing_all_fields_invalid_json = {"field1": "Bob", "field2": "Family", "field3": "Safety"}
         
         missing_field_cases = [
             ("missing name", missing_name_json, self.required_error(["name"])),
             ("missing relationship", missing_relationship_json, self.required_error(["relationship"])),
             ("missing prayer", missing_prayer_json, self.required_error(["prayer"])),
-            ("missing all fields", missing_all_json, self.required_error(self.required_fields)),
+            ("missing all fields (empty)", missing_all_fields_empty_json, self.required_error(self.required_fields)),
+            ("missing all fields (invalid)", missing_all_fields_invalid_json, self.required_error(self.required_fields))
         ]
         
         for title, data, expected_errors in missing_field_cases:
             with self.subTest(case=title):
                 parsed, errors = validate_fields(data, self.required_fields)
                 
-                expected_parsed = {field:value for field, value in data.items() if data[field] is not None}
+                expected_parsed = {field:value for field, value in data.items() if field in self.required_fields}
                 
                 if expected_parsed.get("relationship") is not None:
                     expected_parsed["relationship"] = Relationship.FAMILY
@@ -194,9 +198,9 @@ class TestValidateFields(unittest.TestCase):
                 self.assertListEqual(errors, expected_errors)
 
     def test_invalid_name(self):
-        """Test that an error is provided when name is missing or invalid."""
+        """Test that an error is provided when name is invalid."""
         
-        missing_name_json = generate_person_json(name=None, relationship="Family", prayer="Strength")
+        null_name_json = generate_person_json(allow_null=True, name=None, relationship="Family", prayer="Strength")
         non_string_name_json = generate_person_json(name=123, relationship="Family", prayer="Strength")
         empty_string_name_json = generate_person_json(name="", relationship="Family", prayer="Strength")
         whitespace_name_json = generate_person_json(name="\t \n   ", relationship="Family", prayer="Strength")
@@ -204,7 +208,7 @@ class TestValidateFields(unittest.TestCase):
         name_field = ["name"]
         
         invalid_name_cases = [
-            ("missing name", missing_name_json, self.required_error(name_field)),
+            ("null name", null_name_json, self.string_error(name_field)),
             ("non-string name", non_string_name_json, self.string_error(name_field)),
             ("empty string name", empty_string_name_json, self.non_empty_string_error(name_field)),
             ("whitespace name", whitespace_name_json, self.non_empty_string_error(name_field))
@@ -215,7 +219,60 @@ class TestValidateFields(unittest.TestCase):
         for title, data, expected_errors in invalid_name_cases:
             with self.subTest(case=title):
                 parsed, errors = validate_fields(data, self.required_fields)
-            
+                
+                self.assertDictEqual(parsed, expected_parsed)
+                self.assertListEqual(errors, expected_errors)
+                
+    def test_invalid_relationship(self):
+        """Test that an error is provided when relationship is invalid."""
+        
+        null_relationship_json = generate_person_json(allow_null=True, name="Chris", relationship=None, prayer="Peace")
+        non_string_relationship = generate_person_json(name="Chris", relationship=tuple("Friends"), prayer="Peace")
+        invalid_relationship_type_json = generate_person_json(name="Chris", relationship="Stranger", prayer="Peace")
+        whitespace_invalid_relationship_type_json = generate_person_json(name="Chris", relationship="Classmate\t   ", prayer="Peace")
+        
+        relationship_field = ["relationship"]
+        
+        invalid_relationship_cases = [
+            ("null relationship", null_relationship_json, self.string_error(relationship_field)),
+            ("non-string relationship", non_string_relationship, self.string_error(relationship_field)),
+            ("invalid relationship type", invalid_relationship_type_json, self.valid_relationship_error(relationship_field)),
+            ("whitespace invalid relationship type", whitespace_invalid_relationship_type_json, self.valid_relationship_error(relationship_field))
+        ]
+        
+        expected_parsed = {"name": "Chris", "prayer": "Peace"}
+        
+        for title, data, expected_errors in invalid_relationship_cases:
+            with self.subTest(case=title):
+                parsed, errors = validate_fields(data, self.required_fields)
+                                
+                self.assertDictEqual(parsed, expected_parsed)
+                self.assertListEqual(errors, expected_errors)
+        
+
+    def test_invalid_prayer(self):
+        """Test that an error is provided when prayer is invalid."""
+
+        null_prayer_json = generate_person_json(allow_null=True, name="Sarah", relationship="People I Know", prayer=None)
+        non_string_prayer_json = generate_person_json(name="Sarah", relationship="People I Know", prayer=["Strength"])
+        empty_string_prayer_json = generate_person_json(name="Sarah", relationship="People I Know", prayer="")
+        whitespace_prayer_json = generate_person_json(name="Sarah", relationship="People I Know", prayer="\n   \n")
+        
+        prayer_field = ["prayer"]
+        
+        invalid_prayer_cases = [
+            ("null prayer", null_prayer_json, self.string_error(prayer_field)),
+            ("non-string prayer", non_string_prayer_json, self.string_error(prayer_field)),
+            ("empty string prayer", empty_string_prayer_json, self.non_empty_string_error(prayer_field)),
+            ("whitespace prayer", whitespace_prayer_json, self.non_empty_string_error(prayer_field))
+        ]
+        
+        expected_parsed = {"name": "Sarah", "relationship": Relationship.KNOWN}
+        
+        for title, data, expected_errors in invalid_prayer_cases:
+            with self.subTest(case=title):
+                parsed, errors = validate_fields(data, self.required_fields)
+
                 self.assertDictEqual(parsed, expected_parsed)
                 self.assertListEqual(errors, expected_errors)
 
