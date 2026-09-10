2981a39919cd79cd92c9908708dcbc5dd43d601d
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-09-10 11:01:42 -0700

    Resolve layout shift when demo appears. Add transition to demo on initial load.

--- ipray4u/static/js/index.js
+++ ipray4u/static/js/index.js
@@ -35,9 +35,17 @@ function handlePageShow(event) {
   }
 }
 
+function handleLaunchDemoPlaying() {
+  launchDemoVideo.classList.add('is-ready');
+}
+
 function initDemoVideoListeners() {
   window.addEventListener('pageshow', handlePageShow);
   document.addEventListener('visibilitychange', handleVisibilityChange);
+
+  if (launchDemoVideo) {
+    launchDemoVideo.addEventListener('playing', handleLaunchDemoPlaying);
+  }
 }
 
 function initPage() {
